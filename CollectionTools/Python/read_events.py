dc7986c1c373efae1b3c8b7f296bd73adf3b209a
Jeong Wook Oh <oh.jeongwook@gmail.com>
2020-01-01 19:46:43 -0800

    Code refactored

--- CollectionTools/Python/read_events.py
+++ CollectionTools/Python/read_events.py
@@ -3,17 +3,22 @@ import win32evtlog
 # [Python for Win32 Extensions Help](http://timgolden.me.uk/pywin32-docs/contents.html)
 
 class WindowsEvent:
-    def __init__(self, channel_name = "Application"):
-        self.ChannelName = channel_name
+    def __init__(self, machine_name = '', source_name = "Application"):
+        self.MachineName = machine_name
+        self.SourceName = source_name
 
     def read_log(self):
-        print("Opening : " + self.ChannelName)
+        print("Opening : " + self.SourceName)
 
         # http://timgolden.me.uk/pywin32-docs/win32evtlog__OpenEventLog_meth.html
+        # https://docs.microsoft.com/en-us/windows/win32/eventlog/querying-for-event-source-messages
         # EventLog keys: https://docs.microsoft.com/en-us/windows/desktop/eventlog/eventlog-key
         #  reg query HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog
 
-        log_handle = win32evtlog.OpenEventLog(None, self.ChannelName)
+        log_handle = win32evtlog.OpenEventLog(self.MachineName, self.SourceName)
+
+        if log_handle is None:
+            return
 
         # Path : str
         #     Event log name or Path of an export file
@@ -21,29 +26,31 @@ class WindowsEvent:
         #    EvtOpenChannelPath (1) or EvtOpenFilePath (2)
         # Session=None : PyEVT_HANDLE
         #    Handle to a remote session (see win32evtlog::EvtOpenSession), or None for local machine.
-        win32evtlog.EvtOpenLog('ForwardedEvents', 1 , None)
+        # win32evtlog.EvtOpenLog('ForwardedEvents', 1 , None)
 
         flags= win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
         total = win32evtlog.GetNumberOfEventLogRecords(log_handle)
         print("Total: %d" % total)
 
-        events=1
+        events = 1
         while events:
             # https://docs.microsoft.com/en-us/windows/desktop/api/winbase/nf-winbase-readeventloga
-            events=win32evtlog.ReadEventLog(log_handle, flags, 0)
+            events = win32evtlog.ReadEventLog(log_handle, flags, 0)
             # http://timgolden.me.uk/pywin32-docs/PyEventLogRecord.html
             for event in events:
+                if not event:
+                    break
+
                 print("%s: %s %d" % (event.TimeWritten.Format(), event.SourceName, event.EventID))
-                data = event.StringInserts
-                if data:
-                    for msg in data:
-                        print('\t'+msg)
+                if event.StringInserts:
+                    for string_insert in event.StringInserts:
+                        print('\t' + string_insert)
                     print('')
+
         win32evtlog.CloseEventLog(log_handle)
 
     def check_channel_config(self):
-        channel_config = win32evtlog.EvtOpenChannelConfig(self.ChannelName, None , 0 )
-
+        channel_config = win32evtlog.EvtOpenChannelConfig(self.SourceName, None , 0 )
         # http://timgolden.me.uk/pywin32-docs/win32evtlog_EvtChannelLoggingConfigLogFilePath.html
         (log_file_path, ret) = win32evtlog.EvtGetChannelConfigProperty(channel_config, win32evtlog.EvtChannelLoggingConfigLogFilePath, 0)
 
@@ -52,8 +59,6 @@ class WindowsEvent:
 if __name__ == '__main__':
     import sys
 
-    channel_name = sys.argv[1]
-    windows_event = WindowsEvent(channel_name = channel_name)
-    #windows_event = WindowsEvent(channel_name = "Security")
-    print(windows_event.check_channel_config())
+    source_name = sys.argv[1]
+    windows_event = WindowsEvent(source_name = source_name)
     windows_event.read_log()
